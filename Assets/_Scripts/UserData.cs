ba0768848affe078b01a18328b32720bb81827e9
Oliver Källerfors <idetorka@hotmail.com>
2022-09-23 13:51:21 +0200

    Added UserData Script

---
+++ Assets/_Scripts/UserData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[Serializable]
+public class UserData
+{
+    public string Name{get; set;}
+    public DateTime DateOfBirth{get; set;}
+    public string Email{get; set;}
+}
