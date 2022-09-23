56c50db5e141ff66e099e74e53deabeab0be5e3f
Oliver Källerfors <idetorka@hotmail.com>
2022-09-23 13:50:55 +0200

    Added Comment script

---
+++ Assets/_Scripts/Comment.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[Serializable]
+public class Comment 
+{
+    public DateTime Date{ get; set; }
+    public string Text{ get; set; }
+}
