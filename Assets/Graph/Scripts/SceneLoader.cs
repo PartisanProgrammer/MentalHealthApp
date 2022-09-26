f949fd1fbe35276944e17a9a5f5a097ff64f06d1
Jonathan Jonsson <jonsson.h.jonathan@gmail.com>
2022-09-26 13:44:31 +0200

    Zoom out implemented

--- Assets/Graph/Scripts/SceneLoader.cs
+++ Assets/Graph/Scripts/SceneLoader.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-   public void DebugTest()
-   {
-      Debug.Log("Click");
-   }
+    [SerializeField] string sceneToLoad;
+
+    
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
 }
